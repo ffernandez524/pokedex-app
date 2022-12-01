2ad059e08f745c55c3a9c58ee3b5f77251041695
ffernandez524 <flareniel@gmail.com>
2022-12-01 14:18:47 -0500

    Replace code in script.js
    1) Removed code from previous assignment
    2) created pokemonList array
    3) created pokemon class for later
    3) added 3 pokemon objects to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
-alert("Hello World!");
+//Pokemon class
+let pokemon = {
+    //String
+    name: '',
+    //Number: Height in feet rounded to 10's (Ex: 5.11)
+    height: 0,
+    //Array of Strings that hold each type the pokemon has
+    types: ''
+};
 
-let favoriteFood = "Pizza";
+//Array of Pokemon objects
+let pokemonList = [];
 
-function pizza_time () {
-    document.write(favoriteFood + " time!");
-}
+//Add 3 pokemon to array
+pokemonList.push(
+    { name: 'Zigzagoon', height: 1.04, types : ['Normal'] },
+    { name: "Farfetch'd", height: 2.07, types: ['Normal', 'Flying'] },
+    { name: 'Psyduck', height: 2.07, types: ['Water'] }
+);
+
