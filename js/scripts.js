f3f4235db575e85c33e92cc0d0f8ada4099f373e
ffernandez524 <flareniel@gmail.com>
2022-12-09 20:09:33 -0500

    Wrap pokemonList in IIFE
    1) Implemented pokemonRepository as IIFE for pokemonList
    
    2) Use add function of pokemonRepository to add pokemon
    
    3) Use getAll function and ForEach loop to write each pokemon in the repository

--- js/scripts.js
+++ js/scripts.js
@@ -8,21 +8,39 @@ let pokemon = {
     types: ''
 };
 
-//Array of Pokemon objects
-let pokemonList = [];
+//Pokemon Repository wrapped in IIFE
+let pokemonRepository = (function () {
+    //Array that holds Pokemon Objects
+    let pokemonList = [];
 
-//Add 3 pokemon to array
-pokemonList.push(
-    { name: 'Zigzagoon', height: 1.04, types : ['Normal'] },
-    { name: "Farfetch'd", height: 2.07, types: ['Normal', 'Flying'] },
-    { name: 'Psyduck', height: 2.07, types: ['Water'] }
-);
+    //Get Pokemon List Array
+    function getAll () {
+        return pokemonList;
+    }
+    //Add Pokemon Objects to Array
+    function add (item) {
+        pokemonList.push(item);
+    }
 
-for (i = 0; i < pokemonList.length; i++) {
-    document.write('A wild ' + pokemonList[i].name + ' has appeared! It is ' + pokemonList[i].height + ' feet tall!');
-    if (pokemonList[i].height > 2) {
+    return {
+        getAll: getAll,
+        add: add
+    }
+})();
+
+//Add Pokemon to pokemonRepository
+pokemonRepository.add( { name: 'Zigzagoon', height: 1.04, types : ['Normal'] } );
+pokemonRepository.add( { name: "Farfetch'd", height: 2.07, types: ['Normal', 'Flying'] } );
+pokemonRepository.add( { name: 'Psyduck', height: 2.07, types: ['Water'] } );
+pokemonRepository.add( { name: 'Snorlax', height: 6.11, types: ['Normal'] } );
+
+//Write each pokemon in the repository
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write('A wild ' + pokemon.name + ' has appeared! It is ' + pokemon.height + ' feet tall!');
+
+    if (pokemon.height > 3) {
         document.write(' Wow, that\'s big!<br>');
     } else {
         document.write('<br>');
     }
-}
+});
