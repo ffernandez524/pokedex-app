827edb5048e1167b9148cacec4bfacc13dd02dcd
ffernandez524 <flareniel@gmail.com>
2023-01-12 20:59:13 -0500

    1.8 Task
    1) Added flexbox styling to Webpage
    2) Added Modal functionality that pops up a window when a user clicks on a pokemon, displaying details.

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,7 @@ let pokemonRepository = (function () {
         return pokemonList;
     }
     
+    //Add a button for each pokemon in the retrieved list
     function addListItem(pokemon) {
         let documentList = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
@@ -31,13 +32,14 @@ let pokemonRepository = (function () {
         documentList.appendChild(listItem);
     }
 
+    //Loads a list of pokemon from the Online API 
     function loadList(){
         return fetch(apiUrl).then(function (response) {
             return response.json();
         }).then(function (json) {
             json.results.forEach(function (item) {
                 let pokemon = {
-                    name: item.name,
+                    name: item.name.charAt(0).toUpperCase() + item.name.slice(1),
                     detailsUrl: item.url
                 };
                 add(pokemon);
@@ -47,6 +49,7 @@ let pokemonRepository = (function () {
         })
     }
 
+    //Load details of pokemon from online API after user clicks on a button
     function loadDetails (item) {
         let url = item.detailsUrl;
         return fetch(url).then(function (response) {
@@ -60,6 +63,13 @@ let pokemonRepository = (function () {
         });
     }
 
+    //Used by showDetails modal function to close/hide the modal
+    function hideModal () {
+        let modalContainer = document.querySelector('#modal-container');
+        modalContainer.classList.remove('is-visible');
+    }
+
+
     function buttonClick(button, pokemon) {
         button.addEventListener('click', function(event) {
             showDetails(pokemon);
@@ -68,8 +78,58 @@ let pokemonRepository = (function () {
 
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function () {
-            console.log(pokemon);
+            let modalContainer = document.querySelector('#modal-container');
+      
+            //Clear all existing modal content
+            modalContainer.innerHTML = '';
+      
+            let modal = document.createElement('div');
+            modal.classList.add('modal');
+        
+            //Add the new modal content
+            let closeButtonElement = document.createElement('button');
+            closeButtonElement.classList.add('modal-close');
+            closeButtonElement.innerText = 'Close';
+            closeButtonElement.addEventListener('click', hideModal);
+          
+            let titleElement = document.createElement('h1');
+            titleElement.innerText = pokemon.name;
+            
+            //Display image of pokemon
+            let imageElement = document.createElement('img');
+            imageElement.src = pokemon.imageUrl;
+            imageElement.innerText = pokemon.name;
+
+            //Display description of pokemon
+            let contentElement = document.createElement('p');
+            contentElement.innerText = pokemon.name + ' is ' + pokemon.height + ' feet tall!\n';
+            
+            //Add new elements to container
+            modal.appendChild(closeButtonElement);
+            modal.appendChild(titleElement);
+            modal.appendChild(imageElement);
+            modal.appendChild(contentElement);
+            modalContainer.appendChild(modal);
+            
+            //Close modal if clicked outside of container
+            modalContainer.addEventListener('click', (e) => {
+                let target = e.target;
+                if (target === modalContainer) {
+                    hideModal();
+                }
+            });
+
+            //Press Escape to hideModal
+            window.addEventListener('keydown', (e) => {
+                let modalContainer = document.querySelector('#modal-container');
+                if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+                    hideModal();
+                }
+            });
+            modalContainer.classList.add('is-visible');
         });
+
+        
     }
 
     //Add Pokemon Objects to Array
@@ -120,7 +180,8 @@ let pokemonRepository = (function () {
         add: add,
         showDetails: showDetails,
         loadList: loadList,
-        loadDetails: loadDetails
+        loadDetails: loadDetails,
+        hideModal: hideModal,
     }
 })();
 
